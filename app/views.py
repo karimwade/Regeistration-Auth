bcc62a8a1016d075799e198d4cf221465a3e5569
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-07-11 18:31:30 +0000

    Ajout des messages d'erreur de No matching password

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def homepage(request):
@@ -15,9 +16,12 @@ def signup(request):
         if password == re_password:
             my_user = User.objects.create_user(username,email,password)
             my_user.save()
+            return redirect("login")
         else:
+            messages.info(request,"The password entered and the confirmation does not match")
             print("Password no matching")
         print(username,email,password,re_password)
+
     return render(request,"signup.html")
 
 def login(request):
