4f150fbc3a9b7c0e22c93d45c952421dcced7ec4
Papa Abdou Karim WADE <abdou.karim.info@gmail.com>
2023-07-11 01:37:56 +0000

    Mise en place de l'inscription (Signup)

--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 def homepage(request):
     return render(request,"home.html")
 
 def signup(request):
+    if request.method=='POST':
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        re_password = request.POST.get("re_password")
+        
+        if password == re_password:
+            my_user = User.objects.create_user(username,email,password)
+            my_user.save()
+        else:
+            print("Password no matching")
+        print(username,email,password,re_password)
     return render(request,"signup.html")
 
 def login(request):
